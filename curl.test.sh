edecc78b8176bd0916b19853d13e190d68fecc30
yuxin862 <97368794+yuxin862@users.noreply.github.com>
2025-05-23 15:27:07 -0700

    Update curl.test.sh

--- curl.test.sh
+++ curl.test.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 # Check server status
-curl http://localhost:5001/
+curl https://flask-app-hw-860094761515.us-central1.run.app
 
 # Send test POST request with mtcars features
 curl -H "Content-Type: application/json" -X POST \
   -d '{"am":1,"carb":4,"cyl":6,"disp":160,"drat":3.9,"gear":4,"hp":110,"qsec":16.46,"vs":0,"wt":2.62}' \
-  http://localhost:5001/predict_mpg
-
+  https://flask-app-hw-860094761515.us-central1.run.app/predict_mpg
